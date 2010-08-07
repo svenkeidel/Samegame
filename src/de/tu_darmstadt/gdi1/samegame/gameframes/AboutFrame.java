ad72abfeaedf345a4a4beef5faebd6b874551ed4
Sven Keidel <svenkeidel@web.de>
2010-08-07 14:21:48 +0200

    added a I18N example

--- src/de/tu_darmstadt/gdi1/samegame/gameframes/AboutFrame.java
+++ src/de/tu_darmstadt/gdi1/samegame/gameframes/AboutFrame.java
@@ -1,21 +1,42 @@
 package de.tu_darmstadt.gdi1.samegame.gameframes;
 
 import java.awt.Color;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+@SuppressWarnings("serial")
 public class AboutFrame extends JFrame{
+
+	private Locale currentLocale;
+
+	private ResourceBundle messages;
+
 	public AboutFrame(){
 		super("About");
+
+        currentLocale = new Locale("de", "DE");
+
+		messages = 
+			ResourceBundle.getBundle("de.tu_darmstadt.gdi1.samegame.gameframes.AboutBundle", 
+									 currentLocale, 
+									 this.getClass().getClassLoader()); 
 
 		setBackground(Color.lightGray);
 		this.setSize(300,100);
 		this.setLocation(200,300);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		// TODO write something better than this
-		this.getContentPane().add(new JLabel("Made by Seb, Tim, Svenja and Sven"));
+		this.getContentPane().add(new JLabel(
+					messages.getString("AboutText")
+					+ "Seb, Tim, Svenja and Sven"));
 
 		this.setVisible(true);
+	}
+
+	public static void main(String args[]){
+		AboutFrame about = new AboutFrame();
 	}
 }
